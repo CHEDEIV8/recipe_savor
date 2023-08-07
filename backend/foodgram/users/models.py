9f824279e9db18b965d317e046be8b700c3da377
Денис Чередниченко <promo.ekb.cherednichenko.d@gmail.com>
2023-08-07 19:34:43 +0300

    Создал модель user

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError 
+from django.db import models
+
+User = get_user_model()
+
+class Follow(models.Model): 
+    user = models.ForeignKey(User, 
+                             related_name='follower', 
+                             verbose_name='Подписчик', 
+                             on_delete=models.CASCADE) 
+    author = models.ForeignKey(User,
+                               related_name='following', 
+                               verbose_name='Автор', 
+                               on_delete=models.CASCADE) 
+    class Meta:
+        verbos_name = 'Подписка'
+        verbos_name_plural = 'Подписки'
+        ordering = ('id',) 
+        constraints = [ 
+            models.UniqueConstraint(
+                fields=['user', 'author'], 
+                name='uniqe_follow'
+                )
+            ]
+    def __str__(self):
+        return f'Подписчик: {self.user.username} - автор {self.author.username}'
+    
+    def clean(self):
+        if self.user == self.author:
+            raise ValidationError("Вы не можете подписаться на самого себя")
